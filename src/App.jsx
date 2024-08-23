3874e0c1c2dbcc4c6427a4cd29316ed93584443b
Christian Frattini <chrifrat1@gmail.com>
2024-08-23 13:51:39 +0100

    deployment fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import { Button } from "./components/Button";
+
 import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline  ">Porchiddio</h1>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden ">
         <Header />
       </div>
