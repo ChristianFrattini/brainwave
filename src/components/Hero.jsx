e15330225a9c11087b34b287474d5a7d6a311554
Christian Frattini <chrifrat1@gmail.com>
2024-08-23 20:39:24 +0100

    deployment fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import Section from "./Section";
 import { curve, heroBackground, robot } from "../assets";
-import { Button } from "./Button";
+import { Button } from "./Button.jsx";
 import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
 import { heroIcons } from "../constants";
 import { ScrollParallax } from "react-just-parallax";
