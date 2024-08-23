603a97a7bbc0abd7c3d4b5f94445d28942511f80
Christian Frattini <chrifrat1@gmail.com>
2024-08-23 13:44:48 +0100

    header section desktop/mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import { Button } from "./components/button";
+import { Button } from "./components/Button";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline  ">Porchiddio</h1>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden ">
-        <Button className="mt-10 " href="#login">
-          bhop
-        </Button>
+        <Header />
       </div>
       <ButtonGradient />
     </>
